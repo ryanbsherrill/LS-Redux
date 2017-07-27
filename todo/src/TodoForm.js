aae090bff858aadc999a4e0c435dab69b6dfab63
Ryan Sherrill <ryanbsherrill@gmail.com>
2017-07-27 05:15:43 -0400

    todo app part done

---
+++ todo/src/TodoForm.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react';
+import './TodoForm.css';
+
+class TodoForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todo: '',
+      todos: [],
+      //mock data: "dew this", "dew that", "dew nothing", "whatever dewd"
+    };
+    this.getTodo = this.getTodo.bind(this);
+    this.addTodo = this.addTodo.bind(this);
+  }
+  getTodo(event) {
+    this.setState({ todo: event.target.value });
+  }
+  addTodo(event) {
+    event.preventDefault();
+    this.setState({ todos: [...this.state.todos, this.state.todo] });
+    this.setState({ todo: '' });
+    console.log(this.state.todos)
+  }
+  render() {
+    return (
+      <div className="container">
+        <form className="todo-form" onSubmit={ this.addTodo }>
+          <h1>2dews</h1>
+          <div className="input-container">
+            <input className="input-bar" value={ this.state.todo } onChange={ this.getTodo }/>
+            <button className="submit-button" type="submit"></button>
+          </div>
+            <div>
+              <ul className="my-list">
+                { this.state.todos.map((todo, index) => <li key={index}>{todo}</li>) }
+              </ul>
+            </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default TodoForm;
